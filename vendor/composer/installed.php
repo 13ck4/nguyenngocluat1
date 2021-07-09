972710a12ad8adda21f7ad28ad73f6cf2a01fbbf
luat <nguyen-ngoc-luat@datanext.co.jp>
2021-07-09 17:07:44 +0700

    update composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3187a2d83a2b6d8a28b3a5882816bbb8022f142e',
+        'name' => 'macbookpro/again-composer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'macbookpro/again-composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3187a2d83a2b6d8a28b3a5882816bbb8022f142e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
